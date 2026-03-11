768954f11a9265281a1b94e6686b0a445d2e0a57
Pnttrrv <ndhkptr@gmail.com>
2026-03-11 21:29:12 +0700

    belajar object

--- Penumpang-Angkot.js
+++ Penumpang-Angkot.js
@@ -10,7 +10,7 @@ function penumpangNaik(namaPenumpang, penumpang) {
         penumpang[i] = namaPenumpang;
         return penumpang;
       } else if (penumpang[i] === namaPenumpang) {
-        console.log(`Penumpang sudah ada di dalam angkot`);
+        console.log(`${namaPenumpang} sudah ada di dalam angkot`);
         return penumpang;
       } else if (i == penumpang.length - 1) {
         penumpang.push(namaPenumpang);
@@ -20,14 +20,50 @@ function penumpangNaik(namaPenumpang, penumpang) {
   }
 }
 
-function penumpangTurun(namaPenumpang) {
-  for (let i = 0; i < penumpang.length; i++) {
-    if (namaPenumpang === penumpang[i]) {
-      penumpang[i] = undefined;
+function penumpangTurun(namaPenumpang, penumpang) {
+  if (penumpang.length === 0) {
+    console.log("Angkot Kosong!!!");
+    return penumpang;
+  } else {
+    for (let i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        console.log(`${namaPenumpang} tidak ada di dalam angkot!`);
+        return penumpang;
+      }
     }
   }
-  return penumpang;
+  // if (namaPenumpang === penumpang[i]) {
+  //   penumpang[i] = undefined;
+  //   return penumpang;
+  // } else if (namaPenumpang != penumpang[i]) {
+  //   console.log`${namaPenumpang} tidak ada di dalam angkot`;
+  // }
 }
 
-// console.log(penumpangNaik("dika"));
-// console.log(penumpangNaik("naya"));
+let mhs = {
+  nama: "dika",
+  umur: 28,
+  lulus: true,
+  IPSemester: [2.9, 3.1, 3.25, 2.88, 3.04],
+  ipk: function () {
+    let total = this.IPSemester.reduce((acc, nilai) => {
+      return Math.round(((acc + nilai) * 100) / 100) / this.IPSemester.length;
+    }, 0);
+    return total;
+  },
+};
+
+console.log(mhs.ipk());
+
+// function ipk() {
+//   const { IPSemester } = mhs;
+//   let newArr = IPSemester.reduce((acc, nilai) => {
+//     return Math.round(((acc + nilai) * 100) / 100) / IPSemester.length;
+//   }, 0);
+//   console.log(newArr);
+// }
+
+// ipk();
