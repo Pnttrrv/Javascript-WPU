d5e9ebec0accca8e95170fdbde852b5529d638fd
Pnttrrv <ndhkptr@gmail.com>
2026-03-18 20:39:53 +0700

    menambahkan fitur isi undefined terlebih dahulu

--- kasir.js
+++ kasir.js
@@ -4,40 +4,41 @@ function Kasir(operator, produks, harga, kas) {
   this.harga = harga;
   this.kas = kas;
 
-  this.tambahProduk = async (produk) => {
-    const response = await fetch(`fileHarga.json`);
-    const data = await response.json();
-    // console.log(data);
-    let totalProduk = this.produks;
-    let listHarga = this.harga;
-    if (totalProduk.length == 0) {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].namaProduk == produk) {
-          totalProduk.push(produk);
-          listHarga.push(harga);
+  // this.tambahProduk = async (produk) => {
+  //   const response = await fetch(`fileHarga.json`);
+  //   const data = await response.json();
+  //   // console.log(data);
+  //   let totalProduk = this.produks;
+  //   let listHarga = this.harga;
+  //   if (totalProduk.length == 0) {
+  //     for (let i = 0; i < data.length; i++) {
+  //       if (data[i].namaProduk == produk) {
+  //         totalProduk.push(produk);
+  //         listHarga.push(harga);
 
-          return totalProduk;
-        } else {
-          console.log(`${produk} belum tersedia.`);
-          return totalProduk;
-        }
-      }
-    } else {
-      for (let i = 0; i < totalProduk.length; i++) {
-        if (totalProduk[i] == undefined) {
-          totalProduk[i] = produk;
-          listHarga[i] = harga;
-          return totalProduk;
-        } else if (i == totalProduk.length - 1) {
-          totalProduk.push(produk);
-          listHarga.push(harga);
-          return totalProduk;
-        }
-      }
-    }
-  };
+  //         return totalProduk;
+  //       } else {
+  //         console.log(`${produk} belum tersedia.`);
+  //         return totalProduk;
+  //       }
+  //     }
+  //   } else {
+  //     for (let i = 0; i < totalProduk.length; i++) {
+  //       if (totalProduk[i] == undefined) {
+  //         totalProduk[i] = produk;
+  //         listHarga[i] = harga;
+  //         return totalProduk;
+  //       } else if (i == totalProduk.length - 1) {
+  //         totalProduk.push(produk);
+  //         listHarga.push(harga);
+  //         return totalProduk;
+  //       }
+  //     }
+  //   }
+  // };
 
   this.hapusBarang = function (produk) {
+    const emptyIndex = this.produks.indexOf(undefined);
     let totalProduk = this.produks;
     let listHarga = this.harga;
     if (totalProduk.length == 0) {
@@ -54,6 +55,39 @@ function Kasir(operator, produks, harga, kas) {
           return produk;
         }
       }
+    }
+  };
+
+  this.tambahProduk = async (produkInput) => {
+    try {
+      const response = await fetch(`fileHarga.json`);
+      const data = await response.json();
+
+      // 1. Find if the product exists in your JSON data
+      const produkData = data.find((item) => item.namaProduk === produkInput);
+
+      if (!produkData) {
+        console.log(`${produkInput} belum tersedia.`);
+        return this.produks;
+      }
+
+      // 2. Look for the first 'undefined' slot
+      const emptyIndex = this.produks.indexOf(undefined);
+
+      if (emptyIndex !== -1) {
+        // If an undefined slot exists, fill it
+        this.produks[emptyIndex] = produkData.namaProduk;
+        this.harga[emptyIndex] = produkData.harga; // Taking price from JSON
+        console.log(`Mengisi slot kosong di indeks ${emptyIndex}`);
+      } else {
+        // If no undefined slots, push to the end
+        this.produks.push(produkData.namaProduk);
+        this.harga.push(produkData.harga);
+      }
+
+      return this.produks;
+    } catch (error) {
+      console.error("Gagal mengambil data produk:", error);
     }
   };
 
